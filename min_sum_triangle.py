b14869e1e3f950524b75e8cc75920bf1a19d909c
joyrahman <joyrahman@gmail.com>
2015-03-09 22:53:54 -0500

    added reverse int

--- min_sum_triangle.py
+++ min_sum_triangle.py
@@ -13,55 +13,63 @@ The minimum path sum from top to bottom is 11 (i.e., 2 + 3 + 5 + 1 = 11).
 Note:
 Bonus point if you are able to do this using only O(n) extra space, where n is the total number of rows in the triangle.
 '''
-
-
 class Solution:
     # @param triangle, a list of lists of integers
     # @return an integer
     def minimumTotal(self, triangle):
-        sum_triangle = 0
-        index = 0
-        for row in triangle:
-           #print "running on row:{} with index={} value".format(row, index)
-            if (index ==0 and len(row)==1):
-                min_val = row[index]
-                sum_triangle += min_val
-                #print(">(First If) picked {} in row: {} with sum:{}".format(min_val, row,sum_triangle))
-
-
-            elif (index==0 and len(row)==2):
-                min_val = row[index]
-                if row[index+1] < min_val:
-                    min_val = row[index+1]
-                    index = index+1
-
-                sum_triangle += min_val
-                #print(">(Second If) picked {} in row: {} with sum:{}".format(min_val, row, sum_triangle))
-
-
-
-            elif (index==0 and len(row)>2):
-                for i in range(index,index+2):
-                    min_val = row[index]
-                    if row[i] < min_val:
-                        min_val = row[i]
-                        index = i
-                sum_triangle += min_val
-                #print(">(Third for) picked {} in row: {} with sum:{}".format(min_val, row,sum_triangle))
-
-
-            else:
-                min_val = row[index]
-                for i in range(index-1,index+2,1):
-                    if row[i] < min_val:
-                        min_val = row[i]
-                        index = i
-                        #print(">(Forth for) picked {} in row: {}".format(min_val, row))
-                sum_triangle += min_val
-                #print(">(Fourth Loop for) picked {} in row: {} with sum:{}".format(min_val, row,sum_triangle))
-
-
-        return sum_triangle
+        row_length = len(triangle)
+        for i in reversed(range(row_length-1)):
+            for j in range(0, i + 1):
+                triangle[i][j] += min(triangle[i + 1][j], triangle[i + 1][j + 1])
+        return triangle[0][0]
+#
+# class Solution3:
+#     # @param triangle, a list of lists of integers
+#     # @return an integer
+#     def minimumTotal(self, triangle):
+#         sum_triangle = 0
+#         index = 0
+#         for row in triangle:
+#            #print "running on row:{} with index={} value".format(row, index)
+#             if (index ==0 and len(row)==1):
+#                 min_val = row[index]
+#                 sum_triangle += min_val
+#                 #print(">(First If) picked {} in row: {} with sum:{}".format(min_val, row,sum_triangle))
+#
+#
+#             elif (index==0 and len(row)==2):
+#                 min_val = row[index]
+#                 if row[index+1] < min_val:
+#                     min_val = row[index+1]
+#                     index = index+1
+#
+#                 sum_triangle += min_val
+#                 #print(">(Second If) picked {} in row: {} with sum:{}".format(min_val, row, sum_triangle))
+#
+#
+#
+#             elif (index==0 and len(row)>2):
+#                 for i in range(index,index+2):
+#                     min_val = row[index]
+#                     if row[i] < min_val:
+#                         min_val = row[i]
+#                         index = i
+#                 sum_triangle += min_val
+#                 #print(">(Third for) picked {} in row: {} with sum:{}".format(min_val, row,sum_triangle))
+#
+#
+#             else:
+#                 min_val = row[index]
+#                 for i in range(index-1,index+2,1):
+#                     if row[i] < min_val:
+#                         min_val = row[i]
+#                         index = i
+#                         #print(">(Forth for) picked {} in row: {}".format(min_val, row))
+#                 sum_triangle += min_val
+#                 #print(">(Fourth Loop for) picked {} in row: {} with sum:{}".format(min_val, row,sum_triangle))
+#
+#
+#         return sum_triangle
 
 
 
