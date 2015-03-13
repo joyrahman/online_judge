8e9a8618c7f9387e43a094c01e0aceb35726f56e
joyrahman <joyrahman@gmail.com>
2015-03-12 21:11:52 -0500

    number_of_1_bits.py added

---
+++ number_of_1_bits.py
@@ -0,0 +1,27 @@
+'''
+Write a function that takes an unsigned integer and returns the number of 1 bits it has (also known as the Hamming weight).
+
+For example, the 32-bit integer 11 has binary representation 00000000000000000000000000001011, so the function should return 3.
+
+Credits:
+Special thanks to @ts for adding this problem and creating all test cases.
+'''
+class Solution:
+    # @param n, an integer
+    # @return an integer
+    def hammingWeight(self, n):
+        x = bin(n)
+        x = (x[2:]).count("1")
+
+        return x
+
+
+def main():
+    test = 11
+    s = Solution()
+    print s.hammingWeight(test)
+
+
+
+if __name__ == "__main__":
+    main()
